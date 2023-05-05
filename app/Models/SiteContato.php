7ac77fb90988d80a7ce723389be977c3c5d16669
Guilherme <guilherme.sousa009@hotmail.com>
2023-05-05 15:26:29 -0300

    Separando motivo contatos em uma tabela + alterando demais arquivos que trabalham junto ao motivo contatos + populador motivo contatos com alguns dados padrões

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -9,6 +9,6 @@ class SiteContato extends Model
 {
         use HasFactory;
         protected $table = 'site_contatos';
-        protected $fillable = ['nome','telefone','email','motivo_contato','mensagem'];
+        protected $fillable = ['nome','telefone','email','motivo_contatos_id','mensagem'];
 
 }
