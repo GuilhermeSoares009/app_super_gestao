8d014832654243cdb03a63fbcd44a6a720a13f32
Guilherme <guilherme.sousa009@hotmail.com>
2023-05-09 08:57:59 -0300

    Autenticação simples

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -24,6 +25,18 @@ class LoginController extends Controller
         ];
 
         $request->validate($regras,$feedback);
-        print_r($request->all());
+
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        $user = new User();
+        $usuario = $user->where('email',$email)->where('password',$password)->get()->first();
+        
+        if(isset($usuario->name))
+        {
+            echo 'Usuário existe';
+        }else{
+            echo 'Usuário não existe';
+        }
     }
 }
