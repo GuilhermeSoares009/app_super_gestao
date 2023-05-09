5c82b497442c2ffa7e800d7db8b1bf637e6b8b07
Guilherme <guilherme.sousa009@hotmail.com>
2023-05-09 07:32:13 -0300

    Iniciando os trabalhos com login e caminhando para a autenticação do usuario

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\{
     PrincipalController,
     SobreNosController,
     ContatoController,
-    FornecedorController
+    FornecedorController,
+    LoginController
 };
 
 /*
@@ -23,17 +24,15 @@ Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato',   [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato',  [ContatoController::class, 'salvar'])->name('site.contato');
-Route::get('/login',   function(){ return 'Login';})->name('site.login');
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
-Route::prefix('/app')->group(function(){
-    Route::middleware('autenticacao')
-       ->get('/clientes',   function(){ return 'clientes';})
+Route::middleware('autenticacao:padrao')->prefix('/app')->group(function(){
+    Route::get('/clientes',   function(){ return 'clientes';})
         ->name('app.clientes');
-    Route::middleware('autenticacao')
-        ->get('/fornecedores', [FornecedorController::class, 'index'])
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])
         ->name('app.fornecedores');
-    Route::middleware('autenticacao')
-        ->get('/produtos',   function(){ return 'produtos';})
+    Route::get('/produtos',   function(){ return 'produtos';})
         ->name('app.produtos');
 });
 
